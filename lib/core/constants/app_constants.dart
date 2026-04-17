56bfe18ca12c0fa9239f5f3271d38a9e2666dee2
dzikribdr <dzikriabdr@gmail.com>
2026-04-18 05:57:54 +0700

    feat: membuat constant string

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,14 +1,13 @@
 class AppConstants {
   static const String baseUrl = 'http://localhost:8080/v1';
- 
+
   // Auth endpoints
   static const String verifyToken = '/auth/verify-token';
- 
+
   // Product endpoints
   static const String products = '/products';
- 
+
   // Timeout
   static const int connectTimeout = 15000;
   static const int receiveTimeout = 15000;
-
 }
