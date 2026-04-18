1ea0f193428c45dc998b01f4bd16d6bcc34f5203
dzikribdr <dzikriabdr@gmail.com>
2026-04-18 07:01:09 +0700

    fix: fix import class Cart

--- lib/features/dashboard/presentation/pages/dashboard_page.dart
+++ lib/features/dashboard/presentation/pages/dashboard_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:frozen_food_1123150049/core/routes/app_router.dart';
 import 'package:frozen_food_1123150049/features/auth/presentation/providers/auth_provider.dart';
+import 'package:frozen_food_1123150049/features/cart/presentation/pages/cart_page.dart';
+import 'package:frozen_food_1123150049/features/cart/presentation/providers/cart_provider.dart';
 import 'package:frozen_food_1123150049/features/dashboard/presentation/providers/product_provider.dart';
 import 'package:provider/provider.dart';
 
